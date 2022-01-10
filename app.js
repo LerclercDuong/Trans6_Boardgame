738949bef564c9f836abfc7fc53a82927ac2db8f
minhtri <doantri2003@gmail.com>
2022-01-10 21:51:54 +0700

    first commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var session = require('express-session')
 const express= require('express')
 const handlebars= require('express-handlebars')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 5000
 
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
